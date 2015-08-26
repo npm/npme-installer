c881c807bd97ec28b07584953598a69cbd957400
Benjamin Coe <ben@npmjs.com>
2015-08-25 17:39:46 -0700

    porting to the new replicated installer

--- README.md
+++ README.md
@@ -6,18 +6,13 @@ One-step-installer for npmE servers.
 
 ## Supported Platforms
 
-* Ubuntu Trusty
-* Centos 6.5
+Modern versions of Ubuntu, Debian, Centos, Red Hat & Fedora
 
 ## Prerequisites
 
 * provision a VM for one of our supported platforms.
-* ensure that npm and Node.js 0.10.x are installed:
+* ensure that a modern version of node is installed on the server.
   * https://github.com/joyent/node/wiki/installing-node.js-via-package-manager
-* upgrade npm to 2.x.x on the VM you have provisioned:
-  * `sudo npm install npm -g`
-* npm Enterprise must be installed from an account that has passwordless sudo:
-  * as part of the installation process npm Enterprise creates the `npme` user.
 
 ## Installing
 
@@ -27,7 +22,5 @@ npm install npme
 
 ## Updating
 
-```bash
-npm install npme
-npme update
-```
+Access your server via HTTPS on port 8800 and check for updates via
+the management console.
--- bin/npme.js
+++ bin/npme.js
@@ -1,205 +1,65 @@
 #!/usr/bin/env node
 
-var async = require('async'),
-  yargs = require('yargs')
-    .options('u', {
-      alias: 'user',
-      default: 'npme'
-    })
-    .options('g', {
-      alias: 'group',
-      default: 'npme'
-    })
-    .options('s', {
-      alias: 'sudo',
-      default: true
-    })
-    .options('p', {
-      alias: 'platform',
-      describe: 'what platform is the installer being run on? see ./lib/platform.js'
-    })
-    .option('t', {
-      alias: 'tarball-url',
-      describe: 'path to CouchDB binary tarball URL'
-    })
-    .option('r', {
-      alias: 'proxy',
-      describe: 'provide a proxy URL for fetching the npme license.'
-    }),
-  npmePath = '/etc/npme',
-  fs = require('fs'),
-  logger = require('../lib/logger'),
-  npme = require('../lib'),
-  License = require('../lib/license'),
-  path = require('path'),
-  util = new (require('../lib/util'))(),
-  npmeJson = require('npme-ansible/ansible/librarian_roles/bcoe.npme/files/package.json'),
-  commands = {
-    'install': {
-      description: 'install\t\t\tinstall npm Enterprise on a (preferably) blank operating system.\n',
-      command: function(args) {
-        // create a bin for npme.
+var argv = require('yargs')
+  .help('h')
+  .alias('h', 'help')
+  .option('sudo', {
+    alias: 's',
+    description: 'should shell commands be run as sudo user',
+    boolean: true,
+    default: true
+  })
+  .argv
+var chalk = require('chalk')
+var fs = require('fs')
+var path = require('path')
+var request = require('request')
+var spawn = require('child_process').spawn
 
-        // have we already installed?
-        if (fs.existsSync('/etc/npme/.license.json')) {
-          logger.success("npme is already installed, upgrading.");
+var cwd = path.resolve(__dirname, '../')
 
-          // write the current package.json.
-          fs.writeFileSync(
-            path.resolve(npmePath, './package.json'),
-            JSON.stringify(npmeJson, null, 2)
-          );
-
-          // upgrayedd.
-          async.series([
-            function(done) {
-              var command = 'su npme -c "./node_modules/.bin/npm cache clear; ./node_modules/.bin/npm install --registry=https://enterprise.npmjs.com"';
-
-              if (args.sudo.toString() === 'true') command = 'sudo ' + command;
-
-              util.exec(command, {
-                cwd: npmePath
-              }, function() {
-                done();
-              });
-            },
-            function(done) {
-              util.exec('./node_modules/.bin/ndm restart', {cwd: npmePath}, function(err) { done() });
-            }
-          ]);
-        } else {
-          require('../lib')(args);
-        }
-      }
-    },
-    'start': {
-      description: 'start\t\t\tstart npmE and all its services.\n',
-      command: function(arguments) {
-        util.exec('./node_modules/.bin/ndm start', {cwd: npmePath}, function(err) {
-          logger.success('npmE is now running (curl http://localhost:8080).');
-        });
-      }
-    },
-    'stop': {
-      description: 'stop\t\t\tstop npmE services.\n',
-      command: function(arguments) {
-        util.exec('./node_modules/.bin/ndm stop', {cwd: npmePath}, function(err) {});
-      }
-    },
-    'restart': {
-      description: 'restart\t\t\trestart npmE services.\n',
-      command: function(arguments) {
-        util.exec('./node_modules/.bin/ndm restart', {cwd: npmePath}, function(err) {});
-      }
-    },
-    'add-package': {
-      description: 'add-package\t\tadd a package to the package-follower whitelist (add-package jquery).\n',
-      command: function(arguments) {
-        var package = arguments._[1] || '';
-
-        // all defaults to true. you must set it to false ...
-        // if you want to only add specific versions of a package.
-        var all = true;
-
-
-        if(arguments.a !== undefined) all = arguments.a === 'false'?false:arguments.a;
-        if(arguments['all-versions'] !== undefined) all = arguments['all-versions'];
-
-        all = all?'true':'false';
-
-        util.exec('./node_modules/.bin/ndm run-script manage-whitelist add-package -a '+all+' '+ package, {cwd: npmePath}, function(err) {
-          util.exec('sudo chown npme:npme ./whitelist', {cwd: npmePath}, function(err) {});
-        });
-      }
-    },
-    'reset-follower': {
-      description: 'reset-follower\t\treindex from the public registry all packages listed in whitelist.\n',
-      command: function(args) {
-        async.series([
-          function(done) {
-            util.exec('./node_modules/.bin/ndm stop policy-follower', {
-              cwd: npmePath
-            }, function() { done(); });
-          },
-          function(done) {
-            util.exec('rm .sequence', {
-              cwd: path.resolve(npmePath, './node_modules/@npm/policy-follower')
-            }, function() { done(); });
-          },
-          function(done) {
-            util.exec('./node_modules/.bin/ndm start policy-follower', {
-              cwd: npmePath
-            }, function() { done(); });
-          }
-        ], function() {
-          logger.success('reset policy follower.')
-        });
-      }
-    },
-    'update': {
-      description: "update\t\t\tupdate npm Enteprise.\n",
-      command: function(args) {
-        util.exec(npmePath + '/node_modules/.bin/npm cache clear;' + npmePath + '/node_modules/.bin/npm install npme', {
-          cwd: '/tmp'
-        }, function() {
-          logger.success('npm Enterprise was upgraded!');
-        });
-      }
-    },
-    'generate-scripts': {
-      description: "generate-scripts\tre-generate scripts from service.json.\n",
-      command: function(args) {
-        var command = './node_modules/.bin/ndm generate --uid=' + args.user + ' --gid=' + args.group;
-
-        if (args.sudo.toString() === 'true') command = 'sudo ' + command;
-
-        util.exec(command, {cwd: npmePath}, function(err) {
-          logger.success('generated os services.')
-        });
-      }
-    },
-    'update-license': {
-      description: "update-license\t\tre-generate the .license.json file.",
-      command: function(args) {
-        var license = new License({
-          proxy: args.proxy
-        });
-        license.update(function() {});
-      }
-    },
-    'validate-license': {
-      description: 'check license validity (without touching the .license.json file.\n',
-      command: function(args) {
-        var license = new License({
-          proxy: args.proxy
-        });
-        license.interview(function() {
-          // Otherwise we throw.
-          console.log('license is valid.');
-        });
-      }
+exec('cp replicated-license-retrieval.json /etc', argv.sudo, function () {
+  request.get('https://get.replicated.com', function (err, res, content) {
+    if (err) {
+      console.log(chalk.red(err.message))
+      return
     }
-  },
-  usageString = "npm Enterprise. It's npm, but you run it yourself!\n\n";
 
-// generate usage string.
-Object.keys(commands).forEach(function(command) {
-  usageString += commands[command].description;
-});
+    fs.writeFileSync(path.resolve(cwd, './install.sh'), content, 'utf-8')
 
-yargs.usage(usageString);
+    exec('sh install.sh', argv.sudo, function () {
+      console.log(chalk.bold.green('Congrats! Your npm on-site server is now up and running \\o/'))
+      console.log(chalk.bold('\nThere are just a few final steps:\n'))
+      ;[
+        'Access your server via HTTPS on port 8800',
+        'Proceed passed the HTTPS connection security warning (a selfsigned cert is being used initially)',
+        'Upload a custom TLS/SSL cert/key or proceed with the provided self-signed pair.',
+        'Configure your npm instance & click "Save".',
+        'Visit https://docs.npmjs.com/, for information about using npm on-site or contact support@npmjs.com'
+      ].forEach(function (s, i) {
+        console.log(chalk.bold('Step ' + (i + 1) + '.') + ' ' + s)
+      })
+    })
+  })
+})
 
-// display help if command is not recognized.
-if (yargs.argv.help || !commands[yargs.argv._[0]]) {
-  logger.log(yargs.help());
-} else {
-  // update config singleton and run command.
-  var argv = yargs.normalize().argv;
+function exec (command, sudo, cb) {
+  var commands = ['-c']
+  if (sudo) command = 'sudo ' + command
+  commands.push(command)
 
-  commands[yargs.argv._[0]].command(argv);
+  var proc = spawn('sh', commands, {
+    cwd: cwd,
+    env: process.env,
+    stdio: 'inherit'
+  })
+
+  proc.on('close', function (output) {
+    cb()
+  })
 }
 
-process.on('uncaughtException', function(err) {
-  logger.error(err.message);
-  process.exit(0);
-});
+process.on('uncaughtException', function (err) {
+  console.log(chalk.red(err.message))
+  process.exit(0)
+})
