d21fd6323cec5d85be7dfb0300add32912ff5352
Ryan Day <soldair@gmail.com>
2017-02-22 18:13:31 -0800

    feat: saving update doc in scan db

--- cmd/maintenance.js
+++ cmd/maintenance.js
@@ -8,24 +8,60 @@ var cmd = {
 cmd.builder = function (yargs) {
   yargs
     .option('check', {
-      describe: 'we request each doc in the stream from scan to see if check has the same versions (the primary)',
+      describe: 'we request each doc in the stream from scan to see if check has the same versions (the primary).\nexample: https://theprimary:8080',
       demand: true
     })
     .option('scan', {
-      describe: 'we stream all the documents in this database (the replica)',
+      describe: 'we stream all the documents in this database (the replica).\nexample: http://172.17.0.1:5984/registry\nyou can find this by running `sudo docker ps | grep klaemo | sed \'s/->/ /\' | awk \'{print $(NF-2)}\'`',
       demand: true
     })
     .option('data-directory', {
-      describe: 'data directory to store missing tarballs to',
+      describe: 'data directory to store missing tarballs',
       default: '/usr/local/lib/npme/packages'
     })
+    .option('tar-host',{
+      describe:"download tarballs from this host instead of the external url for your npme. most likely the host you use for `check`. example: https://theprimary:8080",
+      default:false
+    })
+    .option('dry-run',{
+      describe:"only print packages that are missing versions instead of attempting to repair them",
+      default:false
+    })
+    //.option('ignore-404',{
+    //  describe:"you may want to update the document with new versions even if the tarball cannot be found.",
+    //  default: false
+    //})
 }
 
 cmd.handler = function (argv) {
   console.log(argv)
+
   var stream = follow(function(data,cb){
-    console.log(data)
-    cb()
+    var message = data.name+'\t'+JSON.stringify(data.versions)
+
+    if(argv['dry-run']) {
+      console.log(message)
+      return cb()
+    }
+
+    follow.repairVersions({
+      db: argv.scan,
+      currentDoc: data.scan,
+      newDoc: data.check,
+      versions: data.versions,
+      tarHost: argv['tar-host']
+    },function(err,data){
+      if(err) {
+        message += "\terror. "+err
+      } else {
+        message += "\trepaired!"
+      }
+
+      console.log(message)
+
+      cb()
+    })
+
   },{
     check: argv.check,
     scan: argv.scan
